09fa9329b1c7b022461f6ae82759f89f4dda7678
Chandler Fang <chandlerfang@gmail.com>
2018-01-26 22:36:23 +1100

    Adding a qa component.

--- apps/eapps/src/app/app.module.ts
+++ apps/eapps/src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { QaComponent } from './qa.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, QaComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
